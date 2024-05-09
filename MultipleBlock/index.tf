9c07209efa3275f656ed7b28a8f68a9161a046ab
TreX-Hub <devendrapkmishra@gmail.com>
2024-05-09 20:24:12 +0530

    Multiple Block in Singular tf File

---
+++ MultipleBlock/index.tf
@@ -0,0 +1,10 @@
+# output "firstOutputBlock" {
+#   value = "Hello From firstOutputBlock"
+# }
+
+# output "secondOutputBlock" {
+#   value = "Hello From secondOutputBlock"
+# }
+# output "thirdOutputBlock" {
+#   value = "Hello From thirdOutputBlock"
+# }
