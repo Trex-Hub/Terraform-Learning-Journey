0469deec4806759f114300c8911b120bdc225f44
TreX-Hub <devendrapkmishra@gmail.com>
2024-05-10 11:15:03 +0530

    Functions -String Function & Standard Compliance Following in Existing Code

--- List Variable/first.tf
+++ List Variable/first.tf
@@ -1,9 +1,18 @@
-variable "users" {
-    type = list(string)
-}
-
 output "printFirst" {
-  value = "First User of the List is ${var.users[2]}"
+#   value = "First User of the List is ${var.users[1]}"
+    value = " ${ join(" and " , var.users) }" 
 }
 
 # Input  = [ "Devendra" , "ByteBard" , "TreX" ] 
+# terraform plan -var 'users=[ "Devendra" , "ByteBard" , "TreX" ]' 
+
+
+output "wordUppper" {
+    value = "${upper(var.users[0])}"
+}
+output "wordLower" {
+    value = "${lower(var.users[0])}"
+}
+output "wordTitle" {
+    value = "${title(var.users[0])}"
+}
