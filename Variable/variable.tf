b50912a2e33ebc2d400ec14ac2f41d9ac44c4c2a
TreX-Hub <devendrapkmishra@gmail.com>
2024-05-10 10:55:31 +0530

    Variable Type Defining

--- Variable/variable.tf
+++ Variable/variable.tf
@@ -1,7 +1,9 @@
 # Terraform only ask's for input of a variable when Default is not Defined.
 variable "username" {
+    type = string
     default = "Guest"
 }
 
 variable "age" {
+    type = number
 }
