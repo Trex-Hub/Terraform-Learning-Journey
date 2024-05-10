ef1efd6985af99b01966a487f82c342e62260611
TreX-Hub <devendrapkmishra@gmail.com>
2024-05-10 10:49:30 +0530

    Accepting multiple Variable values Via Terminal

--- Variable/printName.tf
+++ Variable/printName.tf
@@ -1,3 +1,8 @@
 output "printName" {
-  value = "Hello, ${var.username}"
-}
+  value = "Hello, ${var.username} is ${var.age} years old."
+} 
+
+# Pass single variable value in terminal =
+  # terraform plan -var "username=Devendra"
+# Pass multiple variable value in terminal =
+  # terraform plan -var "username=Devendra" -var "age=20"
--- Variable/variable.tf
+++ Variable/variable.tf
@@ -1,3 +1,7 @@
+# Terraform only ask's for input of a variable when Default is not Defined.
 variable "username" {
+    default = "Guest"
+}
 
+variable "age" {
 }
