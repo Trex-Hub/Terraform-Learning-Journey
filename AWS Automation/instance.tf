4a73ac3a3362d1bc8b5a087fb4ada87eacf94f07
Devendra <devendra@mavonic.com>
2024-06-17 09:20:19 +0530

    Added Code for Security Group

--- AWS Automation/instance.tf
+++ AWS Automation/instance.tf
@@ -1,19 +1,38 @@
-# Creating SSH Key
-resource "aws_key_pair" "key-tf" {
-  key_name   = "key-tf"
-  public_key = file("${path.module}/id_rsa.pub")
-}
+# # Creating SSH Key
+# resource "aws_key_pair" "key-tf" {
+#   key_name   = "key-tf"
+#   public_key = file("${path.module}/id_rsa.pub")
+# }
 
-output "keyName" {
-  value = aws_key_pair.key-tf.key_name
-}
+# output "keyName" {
+#   value = aws_key_pair.key-tf.key_name
+# }
 
-resource "aws_instance" "First-TF-Instance" {
-  ami           = "ami-0cc9838aa7ab1dce7"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.key-tf.key_name
-  tags = {
-    Name = "First-TF-Instance"
+# Creating Security Group
+
+resource "aws_security_group" "allow_tls" {
+  name        = "First-TF-SG"
+  description = "Allow HTTP and SSH inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+
 }
+
+
+
+# resource "aws_instance" "First-TF-Instance" {
+#   ami           = "ami-0cc9838aa7ab1dce7"
+#   instance_type = "t2.micro"
+#   key_name      = aws_key_pair.key-tf.key_name
+#   tags = {
+#     Name = "First-TF-Instance"
+#   }
+# }
 
