3770d243f6cd02aa8342effc7ba1e463dbee806f
Devendra <devendra@mavonic.com>
2024-06-18 15:31:53 +0530

    chore(ignore): setup .gitignore

--- AWS Automation/instance.tf
+++ AWS Automation/instance.tf
@@ -1,38 +1,10 @@
-# # Creating SSH Key
-# resource "aws_key_pair" "key-tf" {
-#   key_name   = "key-tf"
-#   public_key = file("${path.module}/id_rsa.pub")
-# }
-
-# output "keyName" {
-#   value = aws_key_pair.key-tf.key_name
-# }
-
-# Creating Security Group
-
-resource "aws_security_group" "allow_tls" {
-  name        = "First-TF-SG"
-  description = "Allow HTTP and SSH inbound traffic"
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+# Creating EC2 Instance
+resource "aws_instance" "First-TF-Instance" {
+  ami                    = var.image_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.key-tf.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  tags = {
+    Name = "First-TF-Instance"
   }
-
-
 }
-
-
-
-# resource "aws_instance" "First-TF-Instance" {
-#   ami           = "ami-0cc9838aa7ab1dce7"
-#   instance_type = "t2.micro"
-#   key_name      = aws_key_pair.key-tf.key_name
-#   tags = {
-#     Name = "First-TF-Instance"
-#   }
-# }
-
