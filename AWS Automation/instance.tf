4a9ceb991c806989a6106c2300470e869817d325
Devendra <devendra@mavonic.com>
2024-06-19 15:57:05 +0530

    learn(tf): practising local-exec in dept

--- AWS Automation/instance.tf
+++ AWS Automation/instance.tf
@@ -7,35 +7,63 @@ resource "aws_instance" "First-TF-Instance" {
   tags = {
     Name = "First-TF-Instance"
   }
+  # This will run the script.sh file on the EC2 instance
   user_data = file("${path.module}/script.sh")
 
   connection {
+    # This will connect to the EC2 instance using SSH
+    # This will use the private key to connect to the EC2 instance
+    # This will use the ec2-user as the user
+    # This will use the public IP of the EC2 instance
     type        = "ssh"
     user        = "ec2-user"
     private_key = file("${path.module}/id_rsa")
     host        = self.public_ip
   }
   provisioner "file" {
-    source      = "Readme.MD"
-    destination = "/tmp/Readme.MD"
+    source      = "Readme.MD" # This file should be in the same directory as the instance.tf file
+    destination = "/tmp/Readme.MD" # This is the destination path on the EC2 instance
   }
   provisioner "file" {
-    content     = "This is test content"
+    content     = "This is test content" # This is the content that will be written to the file
     destination = "/tmp/Content.MD"
   }
   provisioner "file" {
-    source     = "test-folder"
+    source      = "test-folder" # This is the folder that will be copied to the EC2 instance
     destination = "/tmp/test-folder/"
   }
 
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} > ip_address.txt " 
-  }
-  provisioner "local-exec" {
-    interpreter = [ 
-      "/usr/bin/python3", "-c"
-     ]
-     command = "print('Hello World')"
+    command = "echo ${self.public_ip} > ip_address.txt " # This will write the public IP of the EC2 instance to the file
   }
 
+  provisioner "local-exec" {
+    interpreter = [
+      "/usr/bin/python3", "-c"
+    ]
+    command = "print('Hello World')" # This will run the python code on the local machine
+  }
+
+  provisioner "local-exec" {
+    # This will run only once when the resource is created
+    # This will create a file with the environment variables
+    command = "env > env.txt"
+    environment = {
+      host = "localhost"
+    }
+    
+  }
+
+  provisioner "local-exec" {
+    when    = destroy # This will only run when the resource is destroyed
+    command = "echo 'Destroying...'"
+  }
+
+  provisioner "local-exec" {
+    # This will run only once when the resource is created
+    command = "echo 'Creating Resources...'"
+  }
+  
 }
+
+
